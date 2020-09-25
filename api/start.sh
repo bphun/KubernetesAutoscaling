287555245292c251bf43f97776a625965526cacf
Brandon Phan <brandon.phan.73@gmail.com>
2020-09-24 21:15:50 -0700

    Moved volume mount path

--- api/start.sh
+++ api/start.sh
@@ -5,6 +5,6 @@ source venv/bin/activate
 N_PROC=$(nproc --all)
 N_WORKERS=$(($N_PROC * 2 + 1))
 
-exec gunicorn -b :5000 --statsd-host=statsd:9125 --statsd-prefix=api --worker-class=gevent --workers=$N_WORKERS --threads=$N_WORKERS --log-level=debug --access-logfile=/etc/api-logs/access.log --error-logfile=/etc/api-logs/error.log server:app
+exec gunicorn -b :5000 --statsd-host=statsd:9125 --statsd-prefix=api --worker-class=gevent --workers=$N_WORKERS --threads=$N_WORKERS --access-logfile=/etc/api-logs/access.log --error-logfile=/etc/api-logs/error.log server:app
 # exec python server.py
 #--worker-connections=1000 
