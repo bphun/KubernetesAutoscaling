2f8c3b47c6ce3ad624d6e92751b734aa8a139130
Brandon Phan <brandon.phan.73@gmail.com>
2021-03-21 01:09:05 -0700

    Add transaction database

--- TransactionAPI/go.mod
+++ TransactionAPI/go.mod
@@ -5,4 +5,7 @@ go 1.16
 require (
 	google.golang.org/grpc v1.35.1
 	google.golang.org/protobuf v1.25.0
+  go.mongodb.org/mongo-driver v1.5.0
 )
+
+replace bphun/k8sAutoscaling/TransactionAPI => ../TransactionAPI/TransactionAPI/
--- TransactionAPI/main.go
+++ TransactionAPI/main.go
@@ -1,26 +1,84 @@
 package main
 
 import (
+	pb "bphun/k8sAutoscaling/TransactionAPI/TransactionAPI"
 	"context"
 	"log"
 	"net"
+	"sync"
 
-	pb "bphun/k8sAutoscaling/TransactionAPI/TransactionAPI"
-
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
 const (
-	port = ":50051"
+	port             = ":50051"
+	CONNECTIONSTRING = "mongodb://localhost:27017"
+	DB               = "db_transaction_manager"
+	TRANSACTIONS     = "col_transaction"
+)
+
+var (
+	clientInstance      *mongo.Client
+	clientInstanceError error
+	mongoOnce           sync.Once
 )
 
 type server struct {
 	pb.UnimplementedTransactionAPIServer
 }
 
+type Transaction struct {
+	InArr     []int32 `bson:"in_arr"`
+	OutArr    []int32 `bson:"out_arr"`
+	ExecTime  int64   `bson:"exec_time"`
+	StartTime uint32  `bson:"start_time"`
+}
+
 func (s *server) SaveTransaction(ctx context.Context, in *pb.TransactionRequest) (*pb.TransactionReply, error) {
-	log.Printf("Received: %v, Result: %v", in.GetInArr(), in.GetOutArr())
-	return &pb.TransactionReply{Message: "Hello"}, nil
+	errorMessage := "Saved transaction"
+
+	err := createTransaction(Transaction{InArr: in.GetInArr(), OutArr: in.GetOutArr(), ExecTime: in.GetExecTime(), StartTime: in.GetStartTime()})
+
+	if err != nil {
+		errorMessage = err.Error()
+	}
+
+	return &pb.TransactionReply{Message: errorMessage}, nil
+}
+
+func getMongoClient() (*mongo.Client, error) {
+	//Perform connection creation operation only once.
+	mongoOnce.Do(func() {
+		// Set client options
+		clientOptions := options.Client().ApplyURI(CONNECTIONSTRING)
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			clientInstanceError = err
+		}
+
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			clientInstanceError = err
+		}
+		clientInstance = client
+	})
+	return clientInstance, clientInstanceError
+}
+
+func createTransaction(task Transaction) error {
+	client, err := getMongoClient()
+	if err != nil {
+		return err
+	}
+	collection := client.Database(DB).Collection(TRANSACTIONS)
+
+	_, err = collection.InsertOne(context.Background(), task)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
